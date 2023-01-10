742c6e9e556fed092c0c690adf6f3ff5aaf6364b
Nayeem Hyder Riddhi <riddhi00121@gmail.com>
2023-01-10 22:11:38 +0600

    youtubeclone commit

--- youtubeclone/server/index.js
+++ youtubeclone/server/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
-const port = 8800 || process.env.PORT
+dotenv.config();
+const port = 8800 || process.env.PORT;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log('MongoDB Connected'))
+.catch((err) => console.log(err));
 
 app.listen(port, () => {
    console.log('Connected to port ' + port);
